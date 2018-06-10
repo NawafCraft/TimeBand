c2d9395b34d3a3ace73f07e4b6bf2ff7af9d69eb
Nawaf_Craft1b <Nawafmg97@gmail.com>
2018-06-10 03:51:14 +0300

    Update BanTime.php

--- src/NawafPlugin/BanTime.php
+++ src/NawafPlugin/BanTime.php
@@ -7,7 +7,28 @@ use pocketmine\plugin\PluginBase;
 
 class BanTime extends PluginBase {
 
-public function onEnable(){
-
+   public function onEnable(){
+       $this->getLogger()->info("Time Ban Plugin Its Enabled");
+       
+   }
+   
+   public function onCommand(\pocketmine\command\CommandSender $sender, \pocketmine\command\Command $command, string $label, string $args) : bool {
+       if($args[0] == "banTime"){
+            foreach ($this->getAPI($sender)->getCommandos() as $p){
+            if($args[1] !== $p){
+            $sender->sendMessage(join($args, " : "));
+            }
+          }
+       }
+   }
+   
+   public function getAPI($name){
+       if(!is_string($name)){
+       return new API($name->getName());
+       }else{
+       return FALSE;
+       }
+       
+   }
 }
-}
+?>
